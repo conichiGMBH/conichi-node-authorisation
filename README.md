955abf9248e2f1a5afd244da674efb8f903dd514
Marc Horne <me@marcushorne.com>
2017-09-22 13:43:24 +0200

    conichi-node-authorisation v1.0.0

---
+++ README.md
@@ -0,0 +1,42 @@
+# Conichi Node Authorisation
+
+Conichi Node Authorisation allows conichi services and 3rd party partners to make authenticated requests to the conichi API.
+
+
+## Installation
+
+Install conichi and save it in the dependencies list. For example:
+`npm install conichi --save`
+
+
+## Example usage
+
+```
+const conichi = require('conichi');
+
+// Replace these values with the cosumer_key, api_key, session_uuid given to you from conichi
+conichi.init(cosumer_key, api_key, session_uuid);
+
+// Make request
+conichi.request(url, method, body, function(response){
+  console.log(response);
+});
+
+// To upload an image, please make sure you are submitting it as multipart/formdata.
+conichi.upload_image(url, formData, function(response){
+  console.log(response);
+});
+```
+
+Please refer to the documentation for information about the correct url endpoints, HTTP methods and expected body content.
+
+
+## API documentation
+
+The documentation for our API can be found here:
+<https://github.com/conichiGMBH/api-documentation>
+
+
+## Report issues
+
+If you have any issues with this module, please contact <marc.horne@conichi.com>.
---
+++ index.js
@@ -0,0 +1,112 @@
+const request = require('request');
+const crypto = require('crypto');
+
+function init(customer_key, api_secret, session_uuid) {
+  try { 
+    if (!customer_key || !api_secret || !session_uuid) throw 'Incomplete init values';
+  }
+  catch(err) {
+    console.log(`Conichi SDK Error: ${ err }`);
+    return;
+  }
+
+  global.customer_key = customer_key;
+  global.api_secret = api_secret;
+  global.session_uuid = session_uuid;
+}
+
+function request(url, method, body, callback) {
+  try { 
+    if (!url) throw 'You have not set the URL';
+    if (!method) throw 'You have not set the method';
+    if (!global.customer_key || !global.api_secret || !global.session_uuid) throw 'Have you forgot to call the init function first?';
+  }
+  catch(err) { console.log(`Conichi SDK Error: ${ err }`); }
+  
+  const reuqest_body = (body) ? JSON.stringify(body) : '';
+
+  const bodyhash_raw = crypto.createHmac('sha256', global.api_secret).update(reuqest_body).digest('base64');
+  const bodyhash = bodyhash_raw.replace(/\//g, '_').replace(/\+/g, '-');
+
+  const timestamp = Math.floor(Date.now() / 1000);
+  const normalized_string = global.session_uuid + "\n" + method + "\n" + url + "\n" + bodyhash  + "\n" + timestamp;
+
+  const hmac_raw = crypto.createHmac('sha256', global.api_secret).update(normalized_string).digest('base64');
+  const hmac = hmac_raw.replace(/\//g , '_').replace(/\+/g, '-');
+
+  request({
+    url: url,
+    method: method,
+    json: true,
+    body: body,
+    headers: {
+      'User-Agent': 'node.js',
+      'X-Consumer-Key': global.customer_key,
+      'X-Session-UUID': global.session_uuid,
+      'X-HMAC': hmac,
+      'X-HMAC-Version': 'HMAC-SHA256',
+      'X-HMAC-Timestamp': timestamp,
+      'X-Body-Hash': bodyhash
+    }
+  }, function (error, response, body) {
+    try {
+      if (error) throw 'Error making api request';
+    }
+    catch(err) {
+      console.log(`Conichi SDK Error: ${ err }`, error);
+      return;
+    }
+
+    callback(response);
+  });
+};
+
+function upload_image(url, formData, callback) {
+  try { 
+    if (!url) throw 'You have not set the URL';
+    if (!formData) throw 'You have not set formData';
+    if (!global.customer_key || !global.api_secret) throw 'Have you forgot to call the init function first?';
+  }
+  catch(err) { console.log(`Conichi SDK Error: ${ err }`); }
+  
+  const bodyhash_raw = crypto.createHmac('sha256', global.api_secret).update('').digest('base64')
+  const bodyhash = bodyhash_raw.replace(/\//g, '_').replace(/\+/g, '-')
+
+  const timestamp = Math.floor(Date.now() / 1000);
+  const normalized_string = global.session_uuid + "\nPUT\n" + url + "\n" + bodyhash  + "\n" + timestamp;
+
+  const hmac_raw = crypto.createHmac('sha256', global.api_secret).update(normalized_string).digest('base64')
+  const hmac = hmac_raw.replace(/\//g , '_').replace(/\+/g, '-')
+
+  request({
+    url: url,
+    method: 'PUT',
+    body: '',
+    formData: formData,
+    headers: {
+      'User-Agent': 'node.js',
+      'X-Consumer-Key': global.customer_key,
+      'X-Session-Uuid': global.session_uuid,
+      'X-Hmac': hmac,
+      'X-Hmac-Version': 'HMAC-SHA256',
+      'X-Hmac-Timestamp': timestamp,
+      'X-Body-Hash': bodyhash
+    }
+  }, function (error, response, body) {
+    try {
+      if (error) throw 'Error making api request';
+    }
+    catch(err) {
+      console.log(`Conichi SDK Error: ${ err }`, error);
+      return;
+    }
+
+    callback(response);
+  });
+};
+
+module.exports = {
+  init : init(),
+  request : request(),
+  upload_image : upload_image()
+}
